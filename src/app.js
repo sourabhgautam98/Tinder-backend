4f8d7b7b846f83c9db324c9e70384cbb7b9c384e
sourabh gautam <gautamsourabh98.com>
2025-09-03 17:39:02 +0530

    fix: update main entry point in package.json to src/app.js; adjust CORS settings in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config(); 
+require("dotenv").config();
 const connectDb = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -11,8 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,              
+    origin: true,
   })
 );
 
