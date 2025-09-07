32869b38c59ea09d5f46bed82acfa10caabc3020
sourabh gautam <gautamsourabh98.com>
2025-09-07 18:28:09 +0530

    fix: update CORS configuration to use environment variable for frontend URL; remove vercel.json

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,              
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
   })
 );
 
