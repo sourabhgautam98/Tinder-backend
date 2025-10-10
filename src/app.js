6535870c441a013368e4335e62ed10116b694e22
sourabh gautam <gautamsourabh98.com>
2025-10-10 17:46:50 +0530

    fix: simplify CORS middleware configuration

--- src/app.js
+++ src/app.js
@@ -9,12 +9,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,              
-  })
-);
+app.use(cors());
 
 // Routes
 const authRoutes = require("./routes/auth");
